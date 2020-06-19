803d4426f6fddbe0562520da107d6b8960f101a1
babyish23 <65775794+babyish23@users.noreply.github.com>
2020-06-19 17:46:49 +0800

    web combination
    yeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

---
+++ playlist/playlist.js
@@ -0,0 +1,13 @@
+//Youtube Video ID
+let playList = [
+    "CkCpPuCkurg",
+    "wUYtzH2QgcA",
+    "RDHK7SPnGSxLM"
+];
+
+//the time for begin and end
+let playTime = [
+    [63,72],
+    [80,87],
+    [46,57]
+];
